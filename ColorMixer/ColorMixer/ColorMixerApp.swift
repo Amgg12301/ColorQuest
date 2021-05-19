adf032d0b51b6a15b294e48104e967de17b2f580
Amogh Giri <amoghgiri01@gmail.com>
2021-05-19 16:50:18 -0400

    pushing working app

---
+++ ColorMixer/ColorMixer/ColorMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerApp.swift
+//  ColorMixer
+//
+//  Created by Amogh Giri on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorMixer/ColorMixer/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ColorMixer
+//
+//  Created by Amogh Giri on 3/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        HomeView()
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+
